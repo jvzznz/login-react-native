653ffc61a9228a0c18d4ae40ed29952416aa3a6a
jvzznz <jvjv52067@gmail.com>
2025-09-16 16:15:44 -0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,35 @@
+import { StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container:{
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: "#f2f2f2",
+  padding: 20,
+},
+  input: {
+  borderWidth: 1,
+  borderColor: "#ggg",
+  borderRadius: 5,
+  padding: 10,
+  marginBottom: 10,
+  width: "80%",
+  backgroundColor: "#fff",
+},
+bemVindo: {
+  fontSize: 18,
+  marginBottom: 10,
+  color: "green",
+},
+nomeTopo: {
+  fontSize: 20,
+  fontWeight: "bold",
+},
+logo: {
+  width: 100,
+  height: 90,
+  marginBottom:20,
+}
+
+});
